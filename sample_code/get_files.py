45861cbe2039509092c1ca989d930f6dd65f4909
Mayuresh Shelke <shelkem@tcd.ie>
2021-10-14 12:04:21 +0100

    file name changes

--- sample_code/get_files.py
+++ sample_code/get_files.py
@@ -1,5 +1,5 @@
 import requests as re
 file_list = re.get('http://cs7ns1.scss.tcd.ie?shortname=shelkem')
 print(file_list.text)
-filenames = re.get('http://cs7ns1.scss.tcd.ie.index.php?download=noresume_speed&shortname=shelkem&myfilename=2122/shelkem/pi-project2/shelkem-challenge-filenames.csv')
+filenames = re.get('http://cs7ns1.scss.tcd.ie.index.php?download=noresume_speed&shortname=shelkem')
 print(filenames.text)
