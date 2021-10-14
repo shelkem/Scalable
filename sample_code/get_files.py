7c2ca2190d9ff431438bb5725867b6c69c4476e1
Mayuresh Shelke <shelkem@tcd.ie>
2021-10-14 11:55:27 +0100

    get_files.py changes

--- sample_code/get_files.py
+++ sample_code/get_files.py
@@ -1,3 +1,5 @@
 import requests as re
 file_list = re.get('http://cs7ns1.scss.tcd.ie?shortname=shelkem')
 print(file_list.text)
+filenames = re.get('index.php?download=noresume_speed&shortname=shelkem')
+print(filenames.text)
