7517c96b0a534bb59b55e43f18b1cef70eeba0ff
Mayuresh Shelke <shelkem@tcd.ie>
2021-10-14 11:51:22 +0100

    get_files.py commit

--- sample_code/get_files.py
+++ sample_code/get_files.py
@@ -1,3 +1,3 @@
 import requests as re
-file_list = re.get('http://cs7ns1.scss.tcd.ie',params={'shortname':'shelkem'})
+file_list = re.get('http://cs7ns1.scss.tcd.ie?shortname=shelkem')
 print(file_list.text)
