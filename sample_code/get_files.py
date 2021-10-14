9990df09841d756a4e4f12e26efe059cf805dfa7
Mayuresh Shelke <shelkem@tcd.ie>
2021-10-14 13:08:27 +0100

    changes in code to download files

--- sample_code/get_files.py
+++ sample_code/get_files.py
@@ -1,9 +1,10 @@
 import requests as re
-file_list = re.get('http://cs7ns1.scss.tcd.ie?shortname=shelkem')
-print(file_list.text)
+# file_list = re.get('http://cs7ns1.scss.tcd.ie?shortname=shelkem')
 filenames = re.get('http://cs7ns1.scss.tcd.ie/index.php?shortname=shelkem&download=noresume_speed')
-print(filenames.text)
-filedownload = re.get('http://cs7ns1.scss.tcd.ie/index.php?shortname=shelkem&&download=noresume_speed&myfilename=0023a1e023923e99d236ee999954868a1d41e5f7.png')
-file = open('file1.png','wb')
-file.write(filedownload.content)
-file.close()
+filenames = filenames.text.split(',\n')
+for each_file in filenames:
+    filedownload = re.get('http://cs7ns1.scss.tcd.ie/index.php?shortname=shelkem&&download=noresume_speed&myfilename='+each_file)
+    file = open(each_file, 'wb')
+    file.write(filedownload.content)
+    file.close()
+
